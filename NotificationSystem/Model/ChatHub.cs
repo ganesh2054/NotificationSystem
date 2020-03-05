8a0186257cd53d09553927667bc0009a9d8fddd1
lme ganesh <lmeganesh123@gmail.com>
2020-03-05 15:54:02 -0800

    initial commit

---
+++ NotificationSystem/Model/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotificationSystem.Model
+{
+    public class ChatHub:Hub
+    {
+        public async Task SendMessage(string user, string messagee)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, messagee);
+        }
+     
+
+
+    }
+}
---
+++ NotificationSystem/wwwroot/js/Notify.js
@@ -0,0 +1,29 @@
+﻿"use strict";
+var count=0;
+var connection = new signalR.HubConnectionBuilder().withUrl("/ChatHub").build();
+
+connection.on("ReceiveMessage", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = "You have message from " + user +"      as     " + msg;
+    
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    count = count + 1;
+    document.getElementById("messages").appendChild(li);
+    document.getElementById("cc").innerHTML=count;
+});
+
+connection.start().catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+
+    var user = document.getElementById("user").value;
+    var message = document.getElementById("message").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+  
+    event.preventDefault();
+});
